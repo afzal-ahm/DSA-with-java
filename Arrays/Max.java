b00b389dca165f8268cc912650a05b29db070e50
Mohd Afzal <afzalahmad@Mohds-MacBook-Pro.local>
2025-11-05 10:56:47 +0530

    Initial commit

---
+++ Arrays/Max.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Max {
+
+    public static int max(int a)
+    public static void main(String[] args)
+    {
+        System.out.println("Enter the size of an array:");
+        Scanner sc =  new Scanner(System.in);
+        int size = sc.nextInt();
+
+        int[] arr = new int[size];
+        
+
+
+       
+
+
+
+        sc.close();
+    }
+}
